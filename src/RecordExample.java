104e435661e72ceac4489cee2a4961d756fdcdf7
Milan Vishnoi <milan.vish10@gmail.com>
2026-04-25 22:32:00 +0530

    Added example for record

---
+++ src/RecordExample.java
@@ -0,0 +1,34 @@
+//record was introduced in Java 16
+
+public record RecordExample(String name, int age) {
+
+    static int rank;
+
+    public RecordExample {
+        rank = 1;
+        System.out.println("This is compact constructor");
+
+    }
+
+    public RecordExample() {
+        this("No name", 0); //Mandatory to call within a non-compact constructor
+        System.out.println("No args constructor called");
+    }
+
+    public static void main(String[] args) {
+        RecordExample recordObj;
+
+        System.out.println("Creating record with values");
+        recordObj = new RecordExample("Milan", 25);
+        System.out.println("Name:%s Age:%d".formatted(recordObj.name(), recordObj.age()));
+
+        System.out.println("\nCreating record without values(no args constructor)");
+        recordObj = new RecordExample();
+        if (recordObj instanceof RecordExample(String name, int age)) { // Pattern matching (available since Java 21)
+            System.out.println("Name:%s Age:%d".formatted(name, age));
+
+        }
+
+    }
+
+}
