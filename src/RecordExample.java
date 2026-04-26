20f265183eb4f3695a2742451b9d6171d323ed20
Milan Vishnoi <milan.vish10@gmail.com>
2026-04-26 16:29:28 +0530

    Added toString() and equals usage in record(which is by default implemented)

--- src/RecordExample.java
+++ src/RecordExample.java
@@ -21,11 +21,14 @@ public record RecordExample(String name, int age) {
         System.out.println("Creating record with values");
         recordObj = new RecordExample("Milan", 25);
         System.out.println("Name:%s Age:%d".formatted(recordObj.name(), recordObj.age()));
+        System.out.println("Using the defualt toString of record: " + recordObj);
+        System.out.println("Equals method:" + recordObj.equals(new RecordExample("Milan", 25)));
 
         System.out.println("\nCreating record without values(no args constructor)");
         recordObj = new RecordExample();
         if (recordObj instanceof RecordExample(String name, int age)) { // Pattern matching (available since Java 21)
             System.out.println("Name:%s Age:%d".formatted(name, age));
+            System.out.println("Using the defualt toString of record: " + recordObj);
 
         }
 
