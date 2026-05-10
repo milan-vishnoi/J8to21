59deb07ed526d03c7e554cde9adc8d9b471c2ea8
Milan Vishnoi <milan.vish10@gmail.com>
2026-05-10 17:40:36 +0530

    Added `SequencedSet` and Collections.sort() example

--- src/CollectionExamples.java
+++ src/CollectionExamples.java
@@ -15,6 +15,8 @@ public class CollectionExamples {
         // Can change the element
         list.set(1, 4);
         System.out.println("Fixed size list update:" + list);
+        Collections.sort(list);
+        System.out.println("Fixed size list sorted:" + list);
         // list.add(5); Giving the UnsupportedOperationException because the list is fixed size
         list = List.of(3, 4, 5, 6);
         System.out.println("Read only list:" + list);
@@ -23,7 +25,7 @@ public class CollectionExamples {
         System.out.println("----\nSet\n----");
         set = new HashSet<>();
         System.out.printf("Adding one:%b Set:%s\n", set.add("One"), set);
-        System.out.printf("Adding two:%b Set:%s\n", set.add("two"), set);
+        System.out.printf("Adding two:%b Set:%s\n", set.add("Two"), set);
         System.out.printf("Adding One again:%b Set:%s\n", set.add("One"), set);
 
         System.out.println("----\nDeque\n----");
@@ -39,6 +41,20 @@ public class CollectionExamples {
         System.out.printf("\nUpdate 1(ek):%s Map:%s", map.put(1, "Ek"), map);
         System.out.printf("\nPut if absent 2(do):%s Map:%s", map.putIfAbsent(2, "do"), map);
         System.out.printf("\nRemove 2:%s Map:%s", map.remove(2), map);
+        System.out.println("\n-----------------------");
+
+        set.add("Four");
+        set.add("Three");
+        System.out.println("Normal set:" + set);
+        SequencedSet<String> sequencedSet = new LinkedHashSet<>(); // HashSet doesn't implement SequencedSet
+        sequencedSet.add("One");
+        sequencedSet.add("Two");
+        sequencedSet.add("Three");
+        sequencedSet.add("Four");
+        System.out.println("Sequenced Set:" + sequencedSet);
+        System.out.println("Reversed Sequenced Set:" + sequencedSet.reversed());
+        sequencedSet.addFirst("Zero");
+        System.out.println("Squenced Set after zero addition:" + sequencedSet);
 
     }
 
