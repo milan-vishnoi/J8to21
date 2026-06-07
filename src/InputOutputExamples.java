d1832245506092c9764090532426d067c12be9ff
Milan Vishnoi <milan.vish10@gmail.com>
2026-06-08 02:09:12 +0530

    Added example for file reader and writer and also the code for printing the code(commented that part).

--- src/InputOutputExamples.java
+++ src/InputOutputExamples.java
@@ -1,21 +1,50 @@
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.Charset;
 
 public class InputOutputExamples {
 
     public static void main(String[] args) {
+
+        // Binary Data reading and writing
         String inputFile = "src\\resources\\inputBin.txt";
         String outputFile = "src\\resources\\outputBin.txt";
         try (InputStream in = new FileInputStream(inputFile); OutputStream out = new FileOutputStream(outputFile)) {
-            byte[] buffer = new byte[1024];
-            int length;
+            byte[] buffer = new byte[2152];
+            int length = 0;
             System.out.println("Size of file(characters):" + in.available());
             while ((length = in.read(buffer)) != -1) {
                 out.write(buffer, 20, length);
+            }
+        } catch (IOException ex) {
+            System.out.println("Exception occured:" + ex.getMessage());
+        }
+
+        // Charater data reading and writing
+        Charset charset = Charset.forName("UTF-8");
+
+        // To print the code and write it to the outputChar file, uncomment below
+        //inputFile = "src\\InputOutputExamples.java";
+        inputFile = "src\\resources\\inputChar.txt";
+        outputFile = "src\\resources\\outputChar.txt";
+        try (Reader in = new FileReader(inputFile, charset); Writer out = new FileWriter(outputFile)) {
+            char[] buffer = new char[1024];
+            int length = 0;
+            System.out.println("Writing into output file:");
+            while ((length = in.read(buffer)) != - 1) {
+                String text = String.valueOf(buffer);
+                text = text.replace("input file", "output file").replace("reading", "writing");
+                System.out.print(text);
+                out.write(text, 0, length);
+
             }
         } catch (IOException ex) {
             System.out.println("Exception occured:" + ex.getMessage());
