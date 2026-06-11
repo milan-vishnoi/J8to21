f826e6da6fae4e1e0397ce8703fa1fb159446fed
Milan Vishnoi <milan.vish10@gmail.com>
2026-06-12 02:08:28 +0530

    Added example of Serialization

---
+++ src/ObjectStreamExamples.java
@@ -0,0 +1,28 @@
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class ObjectStreamExamples implements Serializable {
+
+    String type;
+    int number;
+
+    public ObjectStreamExamples(int number, String type) {
+        this.number = number;
+        this.type = type;
+    }
+
+    public static void main(String[] args) {
+        ObjectStreamExamples obj = new ObjectStreamExamples(1, "Input Stream");
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("src\\resources\\serializedFile.txt"))) {
+            out.writeObject(obj);
+
+            System.out.println("Successfully write the data into the file");
+        } catch (IOException ex) {
+            System.out.println("Some issue occured:" + ex);
+        }
+    }
+
+}
