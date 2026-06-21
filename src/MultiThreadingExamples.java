6c8c9375714dfd3f611350746f115f85132c4fae
Milan Vishnoi <milan.vish10@gmail.com>
2026-06-21 16:54:53 +0530

    Added example of using the child class of Thread to do multithreading

--- src/MultiThreadingExamples.java
+++ src/MultiThreadingExamples.java
@@ -11,6 +11,34 @@ class ThreadedClass implements Runnable {
 
 }
 
+class ChildThreadClass extends Thread {
+
+    Thread currentThread = Thread.currentThread();
+
+    //Below method is optional to override
+    @Override
+    public void run() {
+        currentThread = Thread.currentThread();
+        System.out.println("Within run of " + this.getClass());
+        System.out.println(toString());
+    }
+
+    @Override
+    public String toString() {
+        String details = """
+                        Thread Metadata: %s 
+                        Thread State: %s
+                        Thread isAlive: %s
+                        Priority: %s
+                         """
+                .formatted(super.toString(), currentThread.getState(), currentThread.isAlive(), currentThread.getPriority());
+
+        return details;
+
+    }
+
+}
+
 public class MultiThreadingExamples {
 
     public static void main(String[] args) {
@@ -32,6 +60,15 @@ public class MultiThreadingExamples {
 
         System.out.println("State of Thread:" + t1.getState() + " " + t2.getState());
 
+        System.out.println("-----\nBy Extending Thread Class\n-----");
+        Thread t3 = new ChildThreadClass();
+        t3.start();
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("Exception Occured:" + e);
+        }
+
         System.out.println("-----\nUsing Lambda Expression\n-----");
         Runnable r = () -> {
             System.out.println("Within the Lambda Expression");
@@ -51,14 +88,15 @@ public class MultiThreadingExamples {
 
         };
 
-        Thread t3 = new Thread(r);
-        t3.start();
+        Thread t4 = new Thread(r);
+        t4.setPriority(8);
+        t4.start();
         try {
             Thread.sleep(4000);
         } catch (Exception e) {
             System.out.println("Exception:" + e);
         }
-        t3.interrupt();
+        t4.interrupt();
 
     }
 
