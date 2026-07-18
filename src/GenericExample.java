3d2bb4cf21390c439296100a350f657b873048a1
Milan Vishnoi <milan.vish10@gmail.com>
2026-07-18 17:15:17 +0530

    Added example of wildcard generics

--- src/GenericExample.java
+++ src/GenericExample.java
@@ -1,5 +1,9 @@
 // Generics were introduced in Java 5
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class GenericExample<T> {
 
     T value;
@@ -23,8 +27,35 @@ public class GenericExample<T> {
 
     public static void main(String[] args) {
         GenericExample<Number> obj = new GenericExample<>(10);
-        obj = new GenericExample<Number>(10.2);
+        obj = new GenericExample<>(10.2);
         GenericExample<String> obj1 = new GenericExample<>("Name");
+
+        //Advanced Generics
+        System.out.println("-----\nAdvanced Genrics\n-----");
+
+        List<? extends Number> readOnlyList;
+        List<Integer> intList = Arrays.asList(10, 20, 30);
+        List<Double> doubleList = Arrays.asList(10.1, 20.2, 30.3);
+
+        readOnlyList = intList;
+        System.out.println("Read only list assigned int list:" + readOnlyList);
+        readOnlyList = doubleList;
+        System.out.println("Read only list assigned double list:" + readOnlyList);
+
+        //Below two line will give compiler error
+        //readOnlyList.add(10);
+        //readOnlyList.add(10.1);
+        System.out.println("");
+        List<? super Number> readWriteList = new ArrayList<>();
+
+        //Below two lines will compiler error
+        //readWriteList = intList;
+        //readWriteList = doubleList;
+        readWriteList.add(10);
+        readWriteList.add(10.1);
+        readWriteList.add(20);
+        readWriteList.add(20.2);
+        System.out.println("Read-Write Number list:" + readWriteList);
 
     }
 
